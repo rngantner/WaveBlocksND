b8762e5a739c21b604628e3c17e033d78692280c
Robert Gantner <rngantner@gmail.com>
2012-06-25 13:07:57 +0200

    HyperCubicShape instantiable. Python Exception handling example.

--- lib/BasisRecursion/HyperCubicShape.cpp
+++ lib/BasisRecursion/HyperCubicShape.cpp
@@ -1,10 +1,23 @@
-#include <boost/python.hpp>
 #include <Eigen/Core>
-using namespace boost::python;
 //#include "boost/python.hpp"
-
 #include "HyperCubicShape.h"
 
+#include <boost/python.hpp>
+#include <boost/tuple/tuple_comparison.hpp>
+using namespace boost::python;
+
+/*
+#ifdef PYTHONMODULE
+    #include <boost/python.hpp>
+    using namespace boost::python;
+    //#include <Python.h>
+    //#include <numpy/arrayobject.h>
+#endif
+*/
+
+// remove after debugging:
+#include <iostream>
+#include <string>
 
 //
 // HyperCubicShape
@@ -21,7 +34,11 @@ bool HyperCubicShape::contains(tuple o){
  * Return list of neighbors of the tuple k.
  */
 list HyperCubicShape::get_neighbours(tuple k, bool forward){
-    return list();
+    list ret;
+    ret.append(0);
+    ret.append(1);
+    ret.append(2);
+    return ret;
 }
 
 
@@ -69,10 +86,11 @@ IndexIterator<ValueType>::IndexIterator(tuple limits_, size_t dim_, bool begin=t
         limits[i] = extract<int>(limits_[i]); // should throw a python exception if type is not int-convertible
 
     // initialize index to correct value (either all zeros for begin or correct value for end)
-    index = Eigen::VectorXi::Zero(dim);
+    index = Eigen::VectorXi::Zero(dim+1);
     if (!begin) {
-        for (size_t i=0; i<=dim; i++)
+        for (size_t i=0; i<dim; i++)
             index[i] = limits[i];
+        index[dim] = 1;
     }
 }
 
@@ -135,6 +153,7 @@ bool operator!=( IndexIterator<ValueType> const &lhs, IndexIterator<ValueType> c
 // boost::python stuff
 //
 
+#ifdef PYTHONMODULE
 BOOST_PYTHON_MODULE(HyperCubicShape) {
 // need init call here, or bp will assume a default constructor exists!
 class_<HyperCubicShape>("HyperCubicShape",init<size_t,tuple,dict,dict>())
@@ -147,4 +166,48 @@ class_<HyperCubicShape>("HyperCubicShape",init<size_t,tuple,dict,dict>())
     .def("__iter__",iterator<HyperCubicShape>())
     ;
 }
+
+#else // no python module (PYTHONMODULE not defined)
+
+int main(int argc, const char *argv[])
+{
+    Py_Initialize();
+    std::cout << "creating tuple" << std::endl;
+    // create variables
+    tuple limtuple = make_tuple(2,2); // 0,1 in each dim
+    std::cout << "tuple created" << std::endl;
+    //std::cout << limtuple[0] << std::endl;
+    list indices;
+    //Eigen::VectorXi a,b,c,d;
+    tuple a,b,c,d;
+    std::cout << "creating indices" << std::endl;
+    a = make_tuple(0,0); indices.append(a);
+    b = make_tuple(1,0); indices.append(b);
+    c = make_tuple(0,1); indices.append(c);
+    d = make_tuple(1,1); indices.append(d);
+    std::cout << "created indices" << std::endl;
+    // example dicts
+    std::cout << "creating lima, lima_inv dicts" << std::endl;
+    dict lima,lima_inv;
+    try {
+    lima[a] = 0; lima_inv[0] = a;
+    lima[b] = 1; lima_inv[1] = b;
+    lima[c] = 2; lima_inv[2] = c;
+    lima[d] = 3; lima_inv[3] = d;
+    } catch (...) {//(const error_already_set&) {
+        PyObject *ptype, *pvalue, *ptraceback;
+        PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+        std::string error = extract<std::string>(pvalue);
+        std::cout << "ERROR: " << error << std::endl;
+    }
+    std::cout << "created lima, lima_inv dicts" << std::endl;
+    std::cout << "instantiating HCS" << std::endl;
+    HyperCubicShape hc(2,limtuple,lima,lima_inv);
+    std::cout << "getting HCS iterator" << std::endl;
+    HyperCubicShape::iterator* it = hc.get_index_iterator_chain();
+    std::cout << it->index << std::endl;
+    return 0;
+}
+
+#endif
 
